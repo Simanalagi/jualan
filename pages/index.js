54660631d327136e0027b5270b1dca94cefff31c
Edwin Dwiansyah <edwindwiansyah@Edwin-no-MacBook-Pro.local>
2022-12-26 13:35:03 +0700

    add text new

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-        <p>Edwin Simanalagi</p>
+        <p>Edwin Simanalagi new</p>
     </>
   )
 }
